6b7227cf45a995a4399045fd443177f3954dc607
dasimems <isaacseun63@gmail.com>
2022-11-22 15:33:22 +0100

    done with the settings page

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -6,7 +6,17 @@ import "../assets/styles/settings.css"
 
 const Settings = () => {
 
-    
+    const [formState,setFormState] = useState({
+        accountName: "",
+        bankName: "",
+        accountNo: ""
+    });
+
+    const [passwordstate, setPasswordState] = useState({
+        passwordOne: "",
+        passwordTwo: "",
+        oldPassword: ""
+    })
 
   return (
     <>
@@ -33,10 +43,114 @@ const Settings = () => {
 
         <Row justify="space-between" className="settings-content">
 
-            <Col span={24} className="input-field">
+            <Col span={24} className="form">
+
+                <div className='form-content'>
+
+                    <label htmlFor='account-name'>Account Name</label>
+                    <input id="account-name" className='account-name' value={formState.accountName} onChange={(e)=>{
+                        setFormState(prevState => {
+                            return({
+                                ...prevState,
+                                accountName: e.target.value
+                            })
+                        })
+
+                    }} placeholder="Your Account Name" />
+
+                </div>
+
+                <div className='form-content'>
+
+                    <label htmlFor='bank-name'>Bank Name</label>
+                    <input id="bank-name" className='bank-name' value={formState.bankName} onChange={(e)=>{
+                        setFormState(prevState => {
+                            return({
+                                ...prevState,
+                                bankName: e.target.value
+                            })
+                        })
+
+                    }} placeholder="Your Bank Name" />
+
+                </div>
+
+                <div className='form-content'>
+
+                    <label htmlFor='account-number'>Account Number</label>
+                    <input id="account-number" className='account-number' value={formState.accountNo} onChange={(e)=>{
+                        setFormState(prevState => {
+                            return({
+                                ...prevState,
+                                accountNo: e.target.value
+                            })
+                        })
+
+                    }} placeholder="Your Account Number" />
+
+                </div>
+
+                <div className="form-content">
+                    <Button className='submit-btn'>
+                        Submit
+                    </Button>
+                </div>
                 
+            </Col>
 
+            <Col span={24} className="form">
 
+                <div className='form-content'>
+
+                    <label htmlFor='old-password'>Old password</label>
+                    <input type="password" id="old-password" className='old-password' value={passwordstate.oldPassword} onChange={(e)=>{
+                        setPasswordState(prevState => {
+                            return({
+                                ...prevState,
+                                oldPassword: e.target.value
+                            })
+                        })
+
+                    }} placeholder="Your old Password" />
+
+                </div>
+
+                <div className='form-content'>
+
+                    <label htmlFor='new-password'>New password</label>
+                    <input type="password" id="new-password" className='new-password' value={passwordstate.passwordOne} onChange={(e)=>{
+                        setPasswordState(prevState => {
+                            return({
+                                ...prevState,
+                                passwordOne: e.target.value
+                            })
+                        })
+
+                    }} placeholder="Your New Password" />
+
+                </div>
+
+                <div className='form-content'>
+
+                    <label htmlFor='new-password-two'>Repeat password</label>
+                    <input type="password" id="new-password-two" className='new-password-two' value={passwordstate.passwordTwo} onChange={(e)=>{
+                        setPasswordState(prevState => {
+                            return({
+                                ...prevState,
+                                passwordTwo: e.target.value
+                            })
+                        })
+
+                    }} placeholder="Repeat new Password" />
+
+                </div>
+
+                <div className="form-content">
+                    <Button className='submit-btn'>
+                        Save
+                    </Button>
+                </div>
+                
             </Col>
 
             
